e161757eb4fea7732048332a52003e9bfdd592a9
kati <fanomezantsoakatiuscia048@gmail.com>
2020-09-17 16:27:08 +0300

    added modal to the delete button

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const listOfBirthday = document.querySelector('.birthday');
 const outerModal = document.querySelector('.outer-modal');
 const innerModal = document.querySelector('.inner-modal');
 const editBtn = document.querySelector('button .edit');
+const editedForm = document.querySelector("form");
+const innerConfirmModal = document.querySelector(".inner-confirm");
+const outerConfirmModal = document.querySelector('.outer-confirm');
 
 
 //fetch data from the url
@@ -21,12 +24,10 @@ async function fetchBirthdayList() {
                       <th scope="row"><img src="${person.picture}"/></th>
                         <td>${person.firstName}</td>
                         <td>${person.lastName}</td>
-                        <td>${person.birthday}</td>
+                        <td id="date">Turns ${person.birthday}</td>
                         <td>${person.id}</td>  
-                        <td><button class= "edit">Edit</button></td>                  
-                        <td>
-                          <button class= "delete" class="btn btn-primary">Delete</button>
-                        </td>
+                        <td><button class= "edit">Edit</button></td> 
+                        <td><button class= "delete">Delete</button></td>                 
                     </tr>
                 </body>
             </div>
@@ -34,6 +35,7 @@ async function fetchBirthdayList() {
     });
   listOfBirthday.innerHTML = html.join('');
 }
+
 
 fetchBirthdayList();
 
@@ -64,7 +66,7 @@ window.addEventListener('click', e => {
             </div>
           </div>
           <div class="col-auto">
-            <button type="submit" class="btn btn-primary mb-2">Add</button>
+            <button type="submit" id= "submit-btn" class="btn btn-primary mb-2">Save</button>
           </div>
           </form>
       `;
@@ -94,10 +96,99 @@ const handleEscapeKey = event => {
 };
 
 
-//Listeners
+window.addEventListener('click', e => {
+  if (e.target.matches(".delete")) {
+    //Function that will allow the users to confirm
+   const openConfirmModal = e => {
+    innerConfirmModal.innerHTML = `
+      <div>
+        <p>Are sure to delete this item</p>
+        <button class="btn" id="accept">Accept</button>
+        <button class= "btn"id="cancel">Cancel</button>
+      </div>
+      `;
+      outerConfirmModal.classList.add('confirm');
+  };
+  openConfirmModal();
+  };
+});
+
+
+//function to close modals
+const closeAddModal = event => {
+  const isItOutside = !event.target.closest(".inner-confirm");
+  if (isItOutside) {
+    outerconfirm.classList.remove("confirm")
+  }
+};
+
+const closeConfirmModal = () => {
+  outerconfirm.classList.remove("confirm");
+}
+const handleKey = event => {
+  if (event.key === 'Escape') {
+    closeConfirmModal();
+  }
+};
+
+const listUpdated = document.querySelector('.list');
+
+const newItems = [];
+
+const handleSubmit = e => {
+  e.preventDefault();//prevent the page from reloading
+  if (e.target.matches(".submit-btn")) {
+    const form = e.currentTarget.item.value;
+    if (!form) return;
+    const item = () => {
+    const html = newItems
+    .map(
+        item => 
+            `
+            <form>
+              <input>${item.firstName.value}</input>
+              <input>${item.lastName.value}</input>
+              <input>${item.birthday.value}</input>
+            </form>
+            `
+    )
+    .join('');
+    listUpdated.innerHTML = html;
+  };
+   newItems.push(item);
+    console.log(form);
+  }
+  listUpdated.dispatchEvent(new CustomEvent('itemsUpdated'));
+};
+
+
+const mirrorToLocalStorage = () => {
+  console.info('mirroring item to local storage');
+  localStorage.setItem("newItems", JSON.stringify(newItems));
+};
+
+const restoreFromLocalStorage = () => {
+  console.info('restoring from LS');
+  const lsItems = JSON.parse(localStorage.getItem('newItems'));
+  //check if there is smthg inside the local storage
+  if (lsItems) {
+      //push has no limit for argument
+      newItems.push(...lsItems);
+      listUpdated.dispatchEvent(new CustomEvent('itemsUpdated'));
+  }
+};
+
+restoreFromLocalStorage();
+
+
+
+//listeners
 window.addEventListener("keydown", handleEscapeKey);
 outerModal.addEventListener("click", handleCloseModal);
-
-
+window.addEventListener("key", handleKey);
+outerConfirmModal.addEventListener('click',closeAddModal);
+window.addEventListener("submit", handleSubmit);
+// listUpdated.addEventListener('itemsUpdated', displayItems);
+listUpdated.addEventListener('itemsUpdated', mirrorToLocalStorage);
 
 
